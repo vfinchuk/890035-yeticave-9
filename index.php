4f5d70f3335a31960ac8c780f1f61747f7eff1b5
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-04-13 13:01:12 +0300

    Исправил вывод цены лота

--- index.php
+++ index.php
@@ -102,24 +102,25 @@ $lots = [
             </div>
             <ul class="lots__list">
                 <?php foreach ($lots as $lot): ?>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="<?= $lot['image-url']; ?>" width="350" height="260" alt="<?= $lot['name']; ?>">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?= $lot['category']; ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot['name']; ?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount"><?= $lot['price']; ?></span>
-                                <span class="lot__cost">цена<b class="rub">р</b></span>
-                            </div>
-                            <div class="lot__timer timer">
-                                12:23
+                    <li class="lots__item lot">
+                        <div class="lot__image">
+                            <img src="<?= $lot['image-url']; ?>" width="350" height="260" alt="<?= $lot['name']; ?>">
+                        </div>
+                        <div class="lot__info">
+                            <span class="lot__category"><?= $lot['category']; ?></span>
+                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot['name']; ?></a>
+                            </h3>
+                            <div class="lot__state">
+                                <div class="lot__rate">
+                                    <span class="lot__amount">Стартовая цена</span>
+                                    <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
+                                </div>
+                                <div class="lot__timer timer">
+                                    12:23
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
+                    </li>
                 <?php endforeach; ?>
             </ul>
         </section>
