62371564c03ac1f975c3fc45a5de62880c21f8e1
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-04-25 17:59:13 +0300

    Поправил условия для полей в таблицах. Написал отдельные запросы для внешних ключей

--- schema.sql
+++ schema.sql
@@ -14,39 +14,54 @@ CREATE TABLE categories (
 CREATE TABLE users (
   id            INT AUTO_INCREMENT PRIMARY KEY,
   create_time   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  email         VARCHAR(128),
-  name          VARCHAR(128),
-  password      VARCHAR(64),
+  email         VARCHAR(128) NOT NULL,
+  name          VARCHAR(128) NOT NULL,
+  password      VARCHAR(64) NOT NULL,
   avatar        VARCHAR(128),
-  contact       VARCHAR(255)
+  contact       VARCHAR(255) NOT NULL
 );
 /**
   Таблица лотов
  */
 CREATE TABLE lots (
   id            INT AUTO_INCREMENT PRIMARY KEY,
-  user_id       INT REFERENCES users(id),
-  category_id   INT REFERENCES categories(id),
+  user_id       INT NOT NULL,
+  category_id   INT NOT NULL,
   create_time   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  end_time      DATETIME,
-  name          VARCHAR(128),
-  content       VARCHAR(128),
-  image         VARCHAR(128),
+  end_time      DATETIME NOT NULL,
+  name          VARCHAR(128) NOT NULL,
+  content       VARCHAR(128) NOT NULL,
+  image         VARCHAR(128) NOT NULL,
   start_price   INT NOT NULL,
-  step_rate     INT
-
-
+  step_rate     INT NOT NULL
 );
+
+ALTER TABLE lots
+  ADD FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE lots
+  ADD FOREIGN KEY (category_id)
+    REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /**
   Таблица ставок на лот
  */
 CREATE TABLE bets (
   id          INT AUTO_INCREMENT PRIMARY KEY,
-  user_id     INT REFERENCES users(id),
-  lot_id      INT REFERENCES lots(id),
+  user_id     INT NOT NULL,
+  lot_id      INT NOT NULL,
   create_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   amount      INT NOT NULL
 );
+
+ALTER TABLE bets
+  ADD FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE bets
+  ADD FOREIGN KEY (lot_id)
+    REFERENCES lots(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /**
   Индексы:
