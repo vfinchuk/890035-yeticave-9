fbf85a478737baa075297e89145cc62491d25f8f
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-04-26 19:04:08 +0300

    Обновил SQL-запросы

--- queries.sql
+++ queries.sql
@@ -7,16 +7,16 @@ INSERT INTO yeticave.categories (name, code)
     VALUE ('cat_name', 'cat_code');
 
  /* inset new user in users table */
-INSERT INTO yeticave.users (email, name, password, contact, avatar)
-    VALUE ('email', 'name', 'password', 'contact', 'avatar');
+INSERT INTO yeticave.users (email, name, password, avatar, contact)
+    VALUE ('email', 'name', 'password', 'avatar', 'contact');
 
  /* inset new lot in lots table */
-INSERT INTO yeticave.lots (user_id, category_id, name, price, image)
-    VALUE ('user_id', 'category_id', 'name', 'price', 'image_url');
+INSERT INTO yeticave.lots (user_id, category_id, name, content, image, start_price)
+    VALUE ('user_id', 'category_id', 'name', 'content', 'image_url', 'start_price');
 
  /* inset new bet in bets table */
-INSERT INTO yeticave.lots (price)
-    VALUE ('price');
+INSERT INTO yeticave.bets (amount)
+    VALUE ('amount');
 
 
 /**
@@ -27,10 +27,10 @@ INSERT INTO yeticave.lots (price)
 SELECT * FROM yeticave.categories;
 
 /* получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории */
-SELECT name, price, image, step_rate
+SELECT name, start_price, image, step_rate
       FROM yeticave.lots
-      WHERE dt_end IS NULL
-      ORDER BY dt_add DESC;
+      WHERE end_time IS NULL
+      ORDER BY create_time DESC;
 
 /* показать лот по его id. Получите также название категории, к которой принадлежит лот; */
   SELECT * FROM  yeticave.lots l INNER JOIN categories c ON l.category_id = c.id WHERE l.id in (1,3,5);
@@ -42,4 +42,4 @@ UPDATE yeticave.lots SET name = 'new-name' WHERE id = 2;
 SELECT * FROM yeticave.bets b
     INNER JOIN yeticave.lots l ON b.lot_id = l.id
     WHERE b.lot_id = 2
-    ORDER BY b.dt_add DESC;
+    ORDER BY b.create_time DESC;
