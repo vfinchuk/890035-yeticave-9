fd27ac4e541f5b452350fac2a8d646c007427c0a
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-04-30 18:19:26 +0300

    Подключил БД. Добавил две функции для работы с БД

--- functions/db.php
+++ functions/db.php
@@ -1,4 +1,12 @@
 <?php
+$yeticave_db = mysqli_connect(
+    $config['db']['host'],
+    $config['db']['user'],
+    $config['db']['password'],
+    $config['db']['db_name']
+);
+mysqli_set_charset($yeticave_db, 'utf8');
+
 
 $isAuth = rand(0, 1);
 $userName = 'Vova';
@@ -112,4 +120,48 @@ function db_get_prepare_stmt($link, $sql, $data = [])
     }
 
     return $stmt;
+}
+
+
+/**
+ * Получение записей из БД
+ *
+ * @param       $link mysqli Ресурс соединения
+ * @param       $sql  string SQL запрос с плейсхолдерами вместо значений
+ * @param array $data Данные для вставки на место плейсхолдеров
+ *
+ * @return array
+ */
+function db_fetch_data($link, $sql, $data = [])
+{
+    $result = [];
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+    mysqli_stmt_execute($stmt);
+    $res = mysqli_stmt_get_result($stmt);
+
+    if ($res) {
+        $result = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    }
+
+    return $result;
+}
+
+/**
+ * Добавление / Обновление / Удаление записей в БД
+ *
+ * @param       $link mysqli Ресурс соединения
+ * @param       $sql  string SQL запрос с плейсхолдерами вместо значений
+ * @param array $data Данные для вставки на место плейсхолдеров
+ *
+ * @return boolean
+ */
+function db_insert_data($link, $sql, $data = []) {
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+    $result = mysqli_stmt_execute($stmt);
+
+    if ($result) {
+        $result = mysqli_insert_id($sql);
+    }
+
+    return $result;
 }
