77fc8b05b6e330f733fd166b1078d3e92d6d5385
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-05-17 00:23:02 +0300

    Добавил погинацию на странице категории

--- category.php
+++ category.php
@@ -5,17 +5,33 @@ include_once(__DIR__ . '/bootstrap.php');
 $category_id = $_GET['id'] ?? null;
 $categories = get_categories($connection);
 $category = get_category($connection, intval($category_id));
-
+$lots = null;
+$pagination = null;
 if ($category_id && $category) {
 
     $title = "Категория - {$category['name']}";
+    $lots_count = count_lots_by_category($connection, intval($category_id));
+    $lots_per_page = 3;
 
-    $lots = get_lots_by_category($connection, intval($category_id));
+    if ($lots_count) {
+        $pagination = pagination($lots_count, $lots_per_page);
+
+        $lots = get_lots_by_category_per_page(
+            $connection,
+            intval($category_id),
+            $lots_per_page,
+            $pagination['offset']
+        );
+
+        $pagination['category_id'] = $category_id;
+    }
+
 
     $content = include_template('category.php', [
         'categories'       => $categories,
         'lots'             => $lots,
-        'current_category' => $category
+        'current_category' => $category,
+        'pagination'       => $pagination
     ]);
 
 } else {
--- templates/category.php
+++ templates/category.php
@@ -13,46 +13,60 @@
     </nav>
     <div class="container">
         <section class="lots">
-            <h2>Все лоты в категории <span>«<?= $current_category['name']; ?>
-                    »</span></h2>
-            <ul class="lots__list">
-                <?php foreach ($lots as $lot): ?>
-                    <li class="lots__item lot">
-                        <div class="lot__image">
-                            <img src="<?= $lot['image'] ?? null; ?>" width="350"
-                                 height="260"
-                                 alt="<?= $lot['name'] ?? null; ?>">
-                        </div>
-                        <div class="lot__info">
+            <h2>Все лоты в категории <span>«<?= $current_category['name']; ?>»</span></h2>
+            <?php if (isset($lots)): ?>
+                <ul class="lots__list">
+                    <?php foreach ($lots as $lot): ?>
+                        <li class="lots__item lot">
+                            <div class="lot__image">
+                                <img src="<?= $lot['image'] ?? null; ?>"
+                                     width="350"
+                                     height="260"
+                                     alt="<?= $lot['name'] ?? null; ?>">
+                            </div>
+                            <div class="lot__info">
                             <span class="lot__category"><?= $lot['category'] ??
                                 null; ?></span>
-                            <h3 class="lot__title"><a class="text-link"
-                                                      href="lot.php?id=<?= $lot['id'] ?>"><?= $lot['name']
-                                    ?? null; ?></a>
-                            </h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?= price_format(intval($lot['start_price'])); ?></span>
-                                </div>
-                                <div class="lot__timer timer <?= is_timer_finishing($lot['end_time'],
-                                    1) ? 'timer--finishing' : ''; ?>">
-                                    <?= time_to_end($lot['end_time']); ?>
+                                <h3 class="lot__title"><a class="text-link"
+                                                          href="lot.php?id=<?= $lot['id'] ?>"><?= $lot['name']
+                                        ?? null; ?></a>
+                                </h3>
+                                <div class="lot__state">
+                                    <div class="lot__rate">
+                                        <span class="lot__amount">Стартовая цена</span>
+                                        <span class="lot__cost"><?= price_format(intval($lot['start_price'])); ?></span>
+                                    </div>
+                                    <div class="lot__timer timer <?= is_timer_finishing($lot['end_time'],
+                                        1) ? 'timer--finishing' : ''; ?>">
+                                        <?= time_to_end($lot['end_time']); ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php else: ?>
+                <p>Еще никто не добавил лоты в категорию</p>
+            <?php endif; ?>
+        </section>
+        <?php if ($pagination && ($pagination['pages_count'] > 1)): ?>
+            <ul class="pagination-list">
+                <li class="pagination-item pagination-item-prev">
+                    <a href="category.php?id=<?= $pagination['category_id']; ?>&page=<?= $pagination['prev_page']; ?>">Назад</a>
+                </li>
+                <?php foreach ($pagination['pages'] as $page): ?>
+                    <li class="pagination-item <?php if ($page
+                        == $pagination['current_page']
+                    ) {
+                        echo 'pagination-item-active';
+                    } ?>">
+                        <a href="category.php?id=<?= $pagination['category_id']; ?>&page=<?= $page; ?>"><?= $page; ?></a>
                     </li>
                 <?php endforeach; ?>
+                <li class="pagination-item pagination-item-next">
+                    <a href="category.php?id=<?= $pagination['category_id']; ?>&page=<?= $pagination['next_page']; ?>">Вперед</a>
+                </li>
             </ul>
-        </section>
-        <ul class="pagination-list">
-            <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
-            <li class="pagination-item pagination-item-active"><a>1</a></li>
-            <li class="pagination-item"><a href="#">2</a></li>
-            <li class="pagination-item"><a href="#">3</a></li>
-            <li class="pagination-item"><a href="#">4</a></li>
-            <li class="pagination-item pagination-item-next"><a
-                        href="#">Вперед</a></li>
-        </ul>
+        <?php endif; ?>
     </div>
 </main>
